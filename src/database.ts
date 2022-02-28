78710b0c756a1c4c34f1c4a2856b703de60c0d47
Ahmed Seifelnasr <aseifeln@sfu.ca>
2022-02-28 21:37:35 +0200

    Setup database connection

---
+++ src/database.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import {Pool} from 'pg';
+
+dotenv.config();
+
+const {
+    POSTGRES_HOST,
+    POSTGRES_USER,
+    POSTGRES_DB,
+    POSTGRES_TEST_DB,
+    POSTGRES_PASSWORD,
+    ENV
+} = process.env;
+
+let client: Pool;
+
+if(ENV === 'dev'){
+    client = new Pool({
+        host: POSTGRES_HOST,
+        user: POSTGRES_USER,
+        database: POSTGRES_DB,
+        password: POSTGRES_PASSWORD
+    })
+}else{
+    client = new Pool({
+        host: POSTGRES_HOST,
+        user: POSTGRES_USER,
+        database: POSTGRES_TEST_DB,
+        password: POSTGRES_PASSWORD
+    })   
+}
+
+export default client;
