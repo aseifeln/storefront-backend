bd5e37fd7fa816fd6e4c1c30f668404285e4ef72
Ahmed Seifelnasr <aseifeln@sfu.ca>
2022-02-28 17:36:31 +0200

    Setup project skeleton

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express'
+
+const app: express.Application = express()
+const port: number = 3000
+
+
+app.get('/', function (req: Request, res: Response) {
+    res.send('Hello World!')
+})
+
+app.listen(port, function () {
+    console.log(`starting app on port: ${port}`)
+})
