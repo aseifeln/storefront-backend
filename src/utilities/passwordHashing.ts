11c95f925276b495f0a50218f76c89c600fa6d66
Ahmed Seifelnasr <aseifeln@sfu.ca>
2022-03-03 14:07:13 +0200

    Create password hashing utility function

---
+++ src/utilities/passwordHashing.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const saltRounds = process.env.SALT_ROUNDS;
+const pepper = process.env.BCRYPT_PASSWORD;
+
+const hashPassword = (password: string) => {
+    return bcrypt.hashSync(
+        password + pepper, 
+        Number(saltRounds)
+      );
+}
+
+export default hashPassword;
