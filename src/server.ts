3508e044caca5718efaa883680c587fb4f07f3ee
Ahmed Seifelnasr <aseifeln@sfu.ca>
2022-03-04 22:35:52 +0200

    Import user routes in the server file

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express'
+import userRoutes from './handlers/user'
 
 const app: express.Application = express()
 const port: number = 3000
 
 app.use(express.json())
+// Expose user routes
+userRoutes(app);
+
 app.get('/', function (req: Request, res: Response) {
     res.send('Hello World!')
 })
